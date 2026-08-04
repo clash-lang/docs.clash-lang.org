d23157261d19bd63bf211820655a479db3b4c4ce
rowanG077 <rowanG077@users.noreply.github.com>
2026-08-04 23:03:08 +0000

    deploy: 93865a0c086d57b6e41a644c524197bbde11386d

--- pr/18/searcher-c2a407aa.js
+++ pr/18/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-0b61026b.js',
+                path_to_root + 'searchindex-6432a04c.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
